d8697f3466c75464c144ef8fc558c2ff6803605d
Seth Burleson <sethbcoding@gmail.com>
2025-11-13 10:37:07 -0500

    add dashstats to statswidget

--- EFTEDashboard/src/app/pages/dashboard/components/statswidget.ts
+++ EFTEDashboard/src/app/pages/dashboard/components/statswidget.ts
@@ -12,7 +12,7 @@ import { DashboardStatsDto } from '@/interfaces/outputDtos/dashboard-stats-dto';
 				<div class="flex justify-between mb-4">
 					<div>
 						<span class="block text-muted-color font-medium mb-4">Games Played</span>
-						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152</div>
+						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">{{ dashStats.GamesPlayed }}</div>
 					</div>
 					<div class="flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
 						<i class="pi pi-shopping-cart text-blue-500 text-xl!"></i>
@@ -27,7 +27,7 @@ import { DashboardStatsDto } from '@/interfaces/outputDtos/dashboard-stats-dto';
 				<div class="flex justify-between mb-4">
 					<div>
 						<span class="block text-muted-color font-medium mb-4">Card Choices</span>
-						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">$2.100</div>
+						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">{{ dashStats.TotalChoices }}</div>
 					</div>
 					<div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
 						<i class="pi pi-dollar text-orange-500 text-xl!"></i>
@@ -42,7 +42,7 @@ import { DashboardStatsDto } from '@/interfaces/outputDtos/dashboard-stats-dto';
 				<div class="flex justify-between mb-4">
 					<div>
 						<span class="block text-muted-color font-medium mb-4">Current winrate</span>
-						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">28441</div>
+						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">{{ dashStats.CurrentWinrate }}</div>
 					</div>
 					<div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
 						<i class="pi pi-users text-cyan-500 text-xl!"></i>
@@ -57,14 +57,14 @@ import { DashboardStatsDto } from '@/interfaces/outputDtos/dashboard-stats-dto';
 				<div class="flex justify-between mb-4">
 					<div>
 						<span class="block text-muted-color font-medium mb-4">Current version</span>
-						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152 Unread</div>
+						<div class="text-surface-900 dark:text-surface-0 font-medium text-xl">{{ dashStats.CurrentVersion }}</div>
 					</div>
 					<div class="flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
 						<i class="pi pi-comment text-purple-500 text-xl!"></i>
 					</div>
 				</div>
-				<span class="text-primary font-medium">85 </span>
-				<span class="text-muted-color">responded</span>
+				<!-- <span class="text-primary font-medium">85 </span>
+				<span class="text-muted-color">responded</span> -->
 			</div>
 		</div>
 	`
