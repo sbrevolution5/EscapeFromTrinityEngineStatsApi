871d27f8ce2340b0ca9f33072b635ee1a1e9d6a4
Seth Burleson <sethbcoding@gmail.com>
2025-11-13 10:38:36 -0500

    add migrations

---
+++ StatisticsApi/Migrations/20251113153831_raritycards.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatisticsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class raritycards : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rarity",
+                table: "PassiveInstances",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rarity",
+                table: "CardInstances",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rarity",
+                table: "PassiveInstances");
+
+            migrationBuilder.DropColumn(
+                name: "Rarity",
+                table: "CardInstances");
+        }
+    }
+}
