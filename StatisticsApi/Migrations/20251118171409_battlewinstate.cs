983633da7a4b81c142734cf8d52c69793c7f5637
Seth Burleson <sethbcoding@gmail.com>
2025-11-18 12:14:15 -0500

    add migration

---
+++ StatisticsApi/Migrations/20251118171409_battlewinstate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatisticsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class battlewinstate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "WonBattle",
+                table: "BattleRecords",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WonBattle",
+                table: "BattleRecords");
+        }
+    }
+}
