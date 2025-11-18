282624236c7f50c70e4b483a097327e117d2975a
Seth Burleson <sethbcoding@gmail.com>
2025-11-18 12:59:22 -0500

    check for any instances of rest before calculating avg

--- StatisticsApi/Services/BattleFetchService.cs
+++ StatisticsApi/Services/BattleFetchService.cs
@@ -38,32 +38,36 @@ namespace StatisticsApi.Services
                 VersionName = versionName,
             };
             var battles = new List<BattleInstanceOutputDto>();
-            var instances = records.Select(b => b.BattleInstance).DistinctBy(b=>b.Id).ToList();
+            var instances = records.Select(b => b.BattleInstance).DistinctBy(b => b.Id).ToList();
             foreach (var instance in instances)
             {
                 var instanceRecords = records.Where(b => b.BattleInstance.Id == instance.Id).ToList();
                 var instanceDto = new BattleInstanceOutputDto();
                 if (instanceRecords.Count != 0)
                 {
-                    instanceDto.AverageCardsPlayed = instanceRecords.Average(b => b.Character1CardsPlayed + b.Character2CardsPlayed + b.Character3CardsPlayed);
-                    instanceDto.AverageDamageDealt = instanceRecords.Average(b => b.Character1CardsPlayed + b.Character2CardsPlayed + b.Character3CardsPlayed);
-                    instanceDto.AverageDamageTaken = instanceRecords.Average(b => GetDamageTaken(b));
-                    instanceDto.AverageDamageTakenWhenInactive = instanceRecords.Where(b => b.CharacterResting != -1).Average(b => GetDamageTaken(b, b.CharacterResting));
-                    instanceDto.AverageFloor = instanceRecords.Average(b => b.FloorEncountered * (b.LevelEncountered + 1));
-                    instanceDto.AverageNumberOfDowns = instanceRecords.Average(GetDowns);
-                    instanceDto.AverageTeamworkGained = instanceRecords.Average(b => b.TeamworkEnd - b.TeamworkStart);
-                    instanceDto.AverageTurnsElapsed = instanceRecords.Average(b => b.RoundsElapsed);
                     instanceDto.Name = instance.Name;
                     instanceDto.Tier = instance.Tier;
                     instanceDto.NumberOfBattles = instanceRecords.Count;
                     instanceDto.Wins = instanceRecords.Where(b => b.WonBattle).Count();
 
-                    if (instanceDto.NumberOfBattles > 0)
+                    instanceDto.AverageCardsPlayed = instanceRecords.Average(b => b.Character1CardsPlayed + b.Character2CardsPlayed + b.Character3CardsPlayed);
+                    instanceDto.AverageDamageDealt = instanceRecords.Average(b => b.Character1CardsPlayed + b.Character2CardsPlayed + b.Character3CardsPlayed);
+                    instanceDto.AverageDamageTaken = instanceRecords.Average(b => GetDamageTaken(b));
+                    var inactiveBattles = instanceRecords.Where(b => b.CharacterResting != -1).ToList();
+                    if (inactiveBattles.Count != 0)
                     {
-                        instanceDto.RestingRatio = instanceRecords.Where(i => i.CharacterResting != -1).Count() / instanceDto.NumberOfBattles;
-                        instanceDto.CharacterDownedRatio = instanceRecords.Where(CharacterDownedInBattle).Count() / instanceDto.NumberOfBattles;
-                        instanceDto.Winrate = instanceDto.Wins / instanceDto.NumberOfBattles;
+
+                        instanceDto.AverageDamageTakenWhenInactive = inactiveBattles.Average(b => GetDamageTaken(b, b.CharacterResting));
                     }
+                    instanceDto.AverageFloor = instanceRecords.Average(b => b.FloorEncountered * (b.LevelEncountered + 1));
+                    instanceDto.AverageNumberOfDowns = instanceRecords.Average(GetDowns);
+                    instanceDto.AverageTeamworkGained = instanceRecords.Average(b => b.TeamworkEnd - b.TeamworkStart);
+                    instanceDto.AverageTurnsElapsed = instanceRecords.Average(b => b.RoundsElapsed);
+
+
+                    instanceDto.RestingRatio = instanceRecords.Where(i => i.CharacterResting != -1).Count() / instanceDto.NumberOfBattles;
+                    instanceDto.CharacterDownedRatio = instanceRecords.Where(CharacterDownedInBattle).Count() / instanceDto.NumberOfBattles;
+                    instanceDto.Winrate = instanceDto.Wins / instanceDto.NumberOfBattles;
                     res.BattleObjects.Add(instanceDto);
                 }
             }
