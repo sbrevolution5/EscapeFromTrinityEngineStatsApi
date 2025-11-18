fc072f59792838efe9a7cdcb35ee4990786e4154
Seth Burleson <sethbcoding@gmail.com>
2025-11-18 11:10:53 -0500

    begin modification of table template to fit battles

--- EFTEDashboard/src/app/pages/dashboard/components/battle-stats-component/battle-stats-component.ts
+++ EFTEDashboard/src/app/pages/dashboard/components/battle-stats-component/battle-stats-component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { BattleStatsOutputDto } from '@/interfaces/outputDtos/battle-stats-output-dto';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Table, TableModule } from "primeng/table";
+import { IconField } from "primeng/iconfield";
+import { InputIcon } from "primeng/inputicon";
+import { MultiSelect } from "primeng/multiselect";
+import { Select } from "primeng/select";
+import { Slider } from "primeng/slider";
+import { DatePipe } from '@angular/common';
+import { Tag } from "primeng/tag";
+import { ProgressBar } from "primeng/progressbar";
 
 @Component({
-  selector: 'app-battle-stats-component',
-  imports: [],
-  templateUrl: './battle-stats-component.html',
-  styleUrl: './battle-stats-component.scss'
+	selector: 'app-battle-stats-component',
+	imports: [TableModule, IconField, InputIcon, MultiSelect, Select, Slider, DatePipe, Tag, ProgressBar],
+	templateUrl: './battle-stats-component.html',
+	styleUrl: './battle-stats-component.scss'
 })
 export class BattleStatsComponent {
-
+	battleStats: BattleStatsOutputDto | undefined;
+	loading: boolean = true;
+	@ViewChild('filter') filter!: ElementRef;
+	onGlobalFilter(table: Table, event: Event) {
+		table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+	}
+	clear(table: Table) {
+		table.clear();
+		this.filter.nativeElement.value = '';
+	}
 }
